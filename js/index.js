337a996cddca041e2cb3e12824d9417d3c10862d
unlikelyobjects <hello@unlikelyobjects.com>
2021-04-18 03:30:03 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -53,9 +53,44 @@ function refreshAt(hours, minutes, seconds) {
   then.setSeconds(seconds);
 
   var timeout = then.getTime() - now.getTime();
-  setTimeout(function() {
+  setTimeout(function () {
     window.location.reload(true);
   }, timeout);
 }
 
 refreshAt(0, 0, 0); // Refresh the page at midnight
+
+// Toggle content
+
+// Show an element
+var show = function (elem) {
+  elem.classList.add('is-visible');
+};
+
+// Hide an element
+var hide = function (elem) {
+  elem.classList.remove('is-visible');
+};
+
+// Toggle element visibility
+var toggle = function (elem) {
+  elem.classList.toggle('is-visible');
+};
+
+// Listen for click events
+document.addEventListener('click', function (event) {
+
+  // Make sure clicked element is our toggle
+  if (!event.target.classList.contains('toggle')) return;
+
+  // Prevent default link behavior
+  event.preventDefault();
+
+  // Get the content
+  var content = document.querySelector(event.target.hash);
+  if (!content) return;
+
+  // Toggle the content
+  toggle(content);
+
+}, false);
