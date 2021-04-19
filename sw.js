7d43b8cae1c6ccd34c8f67b4383ab3c3e85eb413
unlikelyobjects <hello@unlikelyobjects.com>
2021-04-19 23:07:22 +0100

    remove prompt, add caching info

--- sw.js
+++ sw.js
@@ -1,40 +1,72 @@
-// Show add to home screen prompt
-// @ https://stackoverflow.com/questions/43646568/how-do-i-create-an-add-to-home-screen-instruction-page-for-ios-web-apps
+const cacheName = 'anewday-shell-v1';
+const assetsToCache = [
+  './icons/check.svg',
+  './icons/checkbox.svg',
+  './icons/delete-grey.svg',
+  './icons/heart.svg',
+  './icons/home.svg',
+  './icons/info.svg',
+  './icons/minus.svg',
+  './icons/plusBl.svg',
+  './js/HtmlService.js',
+  './js/TodoService.js',
+  './node_modules/sortablejs/modular/sortable.complete.esm.js',
+  './css/style.css',
+  './favicon.ico',
+  './info.html',
+  './index.html',
+  './'
+];
 
-// On page load
-// (function () {
-// Check if iOS
-// if (!(/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream)) { return false; }
+function removeOldCache(key) {
+  if (key !== cacheName) {
+    console.log(`[Service Worker] Removing old cache: ${key}`);
+    return caches.delete(key);
+  }
+}
 
-// Check if already in webapp
-// if (window.navigator.standalone == true) { return false; }
+async function cacheCleanup() {
+  const keyList = await caches.keys();
+  return Promise.all(keyList.map(removeOldCache));
+}
 
-// Check if you already asked them to add to homescreen
-// if (document.cookie.search("alreadAsked") >= 0) { return false; }
-
-// Ask user to add to homescreen
-// document.getElementById("hiddenPrompt").style.display = 'inherit';
-// });
-
-// After clicking a button to dismiss prompt
-// function hidePromptInFuture() {
-// Do not show prompt again
-// document.cookie = "alreadAsked=true; expires=Thu, 18 Dec 2099 12:00:00 UTC";
-// }
-
-
+async function cacheStaticAssets() {
+  const cache = await caches.open(cacheName);
+  return cache.addAll(assetsToCache);
+}
 
 self.addEventListener('install', event => {
-  // console.info('[Service Worker] Installing Service Worker ...', event)
-  return self.clients.claim();
-})
+  console.log('[Service Worker] Installing Service Worker...', event);
+  event.waitUntil(cacheStaticAssets());
+  self.skipWaiting();
+});
 
 self.addEventListener('activate', event => {
-  // console.info('[Service Worker] Activating Service Worker ...', event)
-  return self.clients.claim()
-})
+  console.log('[Service Worker] Activating Service Worker...', event);
+  event.waitUntil(cacheCleanup());
+  return self.clients.claim();
+});
+
+async function networkFirst(request) {
+  try {
+    return await fetch(request);
+  } catch (error) {
+    const cache = await caches.open(cacheName);
+    return cache.match(request);
+  }
+}
+
+async function cacheFirst(request) {
+  try {
+    const cache = await caches.open(cacheName);
+    const response = await cache.match(request);
+    return response || fetch(request);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 self.addEventListener('fetch', event => {
-  // console.info('[Service Worker] Fetching something ...', event)
-  event.respondWith(fetch(event.request))
-})
+  // console.log('[Service Worker] Fetch event: ' + event.request.url);
+  event.respondWith(cacheFirst(event.request));
+});
